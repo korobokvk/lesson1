c1d28bbdc37b90e8c6559394430815eccbdf2dd5
Maksym <maksy.z@integritassolutions.net>
2022-03-22 19:52:13 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const onDivClick = () => {
+    const redBlock = document.getElementById('red-block')
+
+    if (!redBlock.style.backgroundColor || redBlock.style.backgroundColor === 'red') {
+        setTimeout(() => {
+            redBlock.style.backgroundColor = 'green'
+        }, 3000)
+    } else {
+        redBlock.style.backgroundColor = ''
+    }
+}
